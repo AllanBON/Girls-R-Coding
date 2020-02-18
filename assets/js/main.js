0e75244dec9d84e1a96f4925bacbec633086ede2
AllanBON <58807166+AllanBON@users.noreply.github.com>
2020-02-18 12:45:29 +0100

    modifs Mathieu
    ajout fonctionnalité +2 like

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,38 @@ docReady(() => {
     const containerElement = document.querySelector(".js-publish-container")
     const actionElement = document.querySelector(".js-publish-action")
     const messageElement = document.querySelector(".js-publish-message")
-
+	//variable qui permettra de nommer les nouvelles publications avec incrémentation
+	var i = 0;
     // Lorsque je clique sur l'element << actionElement >> je déclenche ..
     actionElement.addEventListener('click', () => {
-
+		
         // .. ces traitements
+		i ++;
         const newItemElement = document.createElement("article");
-        newItemElement.innerHTML = getItemTemplate(messageElement.value, 'Marie', new Date())
+        newItemElement.innerHTML = getItemTemplate(messageElement.value, 'Marie', new Date(),i)
 
         // On insère le nouveau post, mais où ca ? Repondre ici :
         containerElement.insertBefore(newItemElement, containerElement.firstChild);
     });
+	
+	
+	
 });
-
-
 
 
 /* --------------------------------------------------------------------- */
 
-
+//fonction qui déclenche une action lorsqu'on effectue un clic sur une zone bien particulière dans la page
+function likeAction(name){
+    //on récupère le texte déjà présent 
+    var texte = parseInt(document.getElementById(name).innerHTML, 10);
+    
+    //on ajoute 2 à ce texte
+    var number = texte + 2;
+    
+    //on réécrit le texte dans la page avec la modification
+    document.getElementById(name).innerHTML = number;
+}
 
 function docReady(fn) {
     (document.readyState === "complete" || document.readyState === "interactive")
@@ -29,7 +42,7 @@ function docReady(fn) {
         : document.addEventListener("DOMContentLoaded", fn)
 }
 
-function getItemTemplate(message, author, date) {
+function getItemTemplate(message, author, date, num) {
 
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
@@ -51,16 +64,16 @@ function getItemTemplate(message, author, date) {
           </p>
   
           <aside class="feedback">
-            <span class="feedback-item">
-              145 likes
+            <span id="likeSpan${num}" class="feedback-item-numb">
+              0
             </span>
             <span class="feedback-item">
-              1 commentaire
+              Likes 1 commentaire
             </span>
           </aside>
   
           <div class="post__actions">
-            <button type="button" name="like" class="post__button post__button--like">Like</button>
+            <button type="button" name="likeSpan${num}" class="post__button post__button--like" onclick="likeAction(name)">Like</button>
             <button type="button" name="repondre" class="post__button post__button--answer">Répondre</button>
           </div>
         </div>`
